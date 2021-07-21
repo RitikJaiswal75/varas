168a4f93cc34a72e7d23ec81b3c7b18d789a81eb
RitikJaiswal75 <ritik12103032000@gmail.com>
2021-07-21 09:08:46 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavsComponent } from './customComponents/navs/navs.component';
+import { AboutComponent } from './customComponents/about/about.component';
+import { HomeComponent } from './customComponents/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavsComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
